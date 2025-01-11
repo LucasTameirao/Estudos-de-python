f144472f921d356efdf16b9be06ec9f378bba32f
Lucas Tameirao <lucas.atmartins@gmail.com>
2025-01-11 16:31:26 -0300

    #

--- aulas/aula14.py
+++ aulas/aula14.py
@@ -1,2 +1,7 @@
 #faça um algoritmoque leia o preço de um produto e mostre seu novo preço, com 5% de desconto
 
+nomeProduto = input("Digite o nome do produto >>> ")
+precoProduto = float(input(f"Digite o preço do {nomeProduto} >>> "))
+precoProdutoDesconto = precoProduto - (precoProduto * 0.15)
+
+print(f'preço inicial --- R${precoProduto}\nvalor do desconto (15%) --- R${precoProduto * 0.15}\npreço com 15% de desconto --- R${precoProdutoDesconto}')
--- aulas/aula15.py
+++ aulas/aula15.py
@@ -1,1 +1,6 @@
 #faça um algoritmo que leia o salario de funcionario e mostre seu novo salario, com 15% de aumento.
+
+SalarioAtual = int(input("Digite o seu salário atual >>> "))
+SalarioAumento = SalarioAtual + (SalarioAtual * (15/100))
+
+print (f"Salário atual --- R${SalarioAtual}\nSalário pós aumento de 15% --- R${SalarioAumento}")
